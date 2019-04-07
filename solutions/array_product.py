68decab606ece39f65f813b36a52c0401127afd1
adela <codreaadela@gmail.com>
2019-04-07 18:08:17 +0200

    Array of integers product solution without division

---
+++ solutions/array_product.py
@@ -0,0 +1,55 @@
+"""
+Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i.
+
+For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].
+
+Follow-up: what if you can't use division?
+
+Note: This is the no division solution
+"""
+
+import unittest
+
+class Solution:   
+    def array_prod( self, nums ) :
+        
+        if len(nums) == 0:
+            return []
+
+        prod = 1
+        prod_map = {}
+        final_result = []
+
+        for idx in reversed(range(len(nums))):
+            prod_map[idx] = prod
+            prod *= nums[idx]
+
+        prod = 1
+        for idx, number in enumerate(nums):
+            final_result.append(prod * prod_map[idx])
+            prod *= number
+
+        return final_result
+
+class MyTestSuite(unittest.TestCase):
+        
+    def test_given_example(self):
+        self.assertEqual(sol.array_prod([1, 2, 3, 4]), [24, 12, 8, 6])
+    def test_empty_list(self):
+        self.assertEqual(sol.array_prod([]), [])
+    def test_1(self):
+        self.assertEqual(sol.array_prod([3]), [1])
+    def test_2(self):
+        self.assertEqual(sol.array_prod([1, 3, 4]), [12, 4, 3])
+    def test_3(self):
+        self.assertEqual(sol.array_prod([1, 3, 5]), [15, 5, 3])
+    def test_4(self):
+        self.assertEqual(sol.array_prod([6, 8, 9]), [72, 54, 48])
+    def large_test(self):
+        self.assertEqual(sol.array_prod([1, 4, 6, 12, 19, 30]), [164160, 41040, 13680, 8640, 5472 ])
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    unittest.main()
+
