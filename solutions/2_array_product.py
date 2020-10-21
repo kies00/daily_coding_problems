ef5aef6500c6b07e08056e02b2f03173da8b73db
adela <mititeanadela@gmail.com>
2020-10-21 14:46:36 +0200

    update test cases

--- solutions/2_array_product.py
+++ solutions/2_array_product.py
@@ -7,46 +7,53 @@ Follow-up: what if you can't use division?
 
 Note: This is the no division solution
 """
-
 import unittest
 
+
 class Solution:   
-    def array_prod( self, nums ) :
-        
+    def array_prod(self, nums):
         if len(nums) == 0:
             return []
 
         prod = 1
         prod_map = {}
-        final_result = []
-
         for idx in reversed(range(len(nums))):
             prod_map[idx] = prod
             prod *= nums[idx]
 
         prod = 1
+        final_result = []
         for idx, number in enumerate(nums):
             final_result.append(prod * prod_map[idx])
             prod *= number
 
         return final_result
 
+
 class MyTestSuite(unittest.TestCase):
-        
     def test_given_example(self):
         self.assertEqual(sol.array_prod([1, 2, 3, 4]), [24, 12, 8, 6])
+
     def test_empty_list(self):
         self.assertEqual(sol.array_prod([]), [])
+
     def test_1(self):
         self.assertEqual(sol.array_prod([3]), [1])
+
     def test_2(self):
         self.assertEqual(sol.array_prod([1, 3, 4]), [12, 4, 3])
+
     def test_3(self):
-        self.assertEqual(sol.array_prod([1, 3, 5]), [15, 5, 3])
+        self.assertEqual(sol.array_prod([0, 3, 5]), [15, 0, 0])
+
     def test_4(self):
         self.assertEqual(sol.array_prod([6, 8, 9]), [72, 54, 48])
-    def large_test(self):
-        self.assertEqual(sol.array_prod([1, 4, 6, 12, 19, 30]), [164160, 41040, 13680, 8640, 5472 ])
+
+    def test_5(self):
+        self.assertEqual(sol.array_prod([1, 1, 1]), [1, 1, 1])
+
+    def test_6(self):
+        self.assertEqual(sol.array_prod([1, 4, 6, 12, 19, 30]), [164160, 41040, 27360, 13680, 8640, 5472])
 
 
 if __name__ == '__main__':
